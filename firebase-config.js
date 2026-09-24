449e1fea7ba547bec13a7d2ad6fd497358f7583d
sebonalt3 <sebonalt3@gmail.com>
2026-09-24 21:31:52 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.JIHOO_FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyDZVIcATuefAFSyL89XGDRuSwBclY5SclE',
+  authDomain: 'jihoobattle.firebaseapp.com',
+  projectId: 'jihoobattle',
+  storageBucket: 'jihoobattle.firebasestorage.app',
+  messagingSenderId: '829255397705',
+  appId: '1:829255397705:web:d4c7d3e3892ab4e9ce2a98'
+};
